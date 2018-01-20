b8ec355f5a753bd829a64df8552b462029e724d9
Alex Kott <tetlokax@gmail.com>
2018-01-20 15:54:37 +0800

    add switcher component and start route

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import Switcher from './Switcher';
+
 const App = () => (
   <div>
     <header>
       <h1>Mix Block Explorer</h1>
     </header>
+    <Switcher />
   </div>
 );
 
---
+++ src/router.js
@@ -0,0 +1,10 @@
+import Start from './components/Start';
+
+export const START = 'router/START';
+
+export default {
+  [START]: {
+    path: '/',
+    component: Start,
+  },
+}
