521917d5975f9868af80d153b8081e1a646635b5
Alex Kott <tetlokax@gmail.com>
2018-01-20 21:59:35 +0800

    display last 10 blocks

--- src/components/Start.js
+++ src/components/Start.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const Start = () => (
+import { getBlocks } from '../reducers/selectors';
+
+const mapStateToProps = (state) => ({
+  blocks: getBlocks(state),
+});
+
+const Start = ({ blocks = [] }) => (
   <div>
     <h2>Welcome!</h2>
+    <ul>
+      {blocks.map(block => {
+        return block && (
+          <li>
+            Block #: {block.number}<br />
+            Tx #: {block.transactions.length}<br />
+            Size: {block.size}<br />
+            Timestamp: {block.timestamp}<br />
+          </li>
+        )
+      })}
+    </ul>
   </div>
 );
 
-export default Start;
+export default connect(mapStateToProps)(Start);
--- src/reducers/blocks.js
+++ src/reducers/blocks.js
@@ -1,13 +1,17 @@
 import * as t from '../actions/types';
 
-const initialState = {};
+const initialState = [];
 
 export default (state = initialState, { type, payload = {} }) => {
   if (type === t.FETCH_BLOCKS) {
-    return {
+    return [
       ...state,
       ...payload.blocks,
-    };
+    ];
   }
   return state;
+}
+
+export function getBlocks(state) {
+  return state.blocks || [];
 }
