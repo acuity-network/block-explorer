4bd3451cf60e37668f2899bfce26ee19feab8375
Alina Beck <alina.beck@mail.com>
2018-01-22 17:48:33 +0800

    web3-tests: remove some copypasta

--- src/adapters/web3/accounts.test.js
+++ src/adapters/web3/accounts.test.js
@@ -11,14 +11,14 @@ describe('adapters/web3/accounts', () => {
   });
 
   describe('getBalance', () => {
-    it('should reject if getting the block failed', async () => {
+    it('should reject if getting the balance failed', async () => {
       mockEth.getBalance = (a, callback) => callback('rejected');
 
       await expect(web3.getBalance('test', mockGetInstance))
         .rejects.toEqual('rejected');
     });
 
-    it('should resolve with the block number', async () => {
+    it('should resolve with the account balance', async () => {
       mockEth.getBalance = (a, callback) => callback(null, 10);
 
       await expect(web3.getBalance('test', mockGetInstance))
@@ -27,14 +27,14 @@ describe('adapters/web3/accounts', () => {
   });
 
   describe('getTransactionCount', () => {
-    it('should reject if getting the block failed', async () => {
+    it('should reject if getting the transaction count failed', async () => {
       mockEth.getTransactionCount = (a, callback) => callback('rejected');
 
       await expect(web3.getTransactionCount('test', mockGetInstance))
         .rejects.toEqual('rejected');
     });
 
-    it('should resolve with the block number', async () => {
+    it('should resolve with the transaction count', async () => {
       mockEth.getTransactionCount = (a, callback) => callback(null, 10);
 
       await expect(web3.getTransactionCount('test', mockGetInstance))
