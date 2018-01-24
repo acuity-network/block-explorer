18c01092a91ab5c4f6eb67e80b74155649fc639d
Alina Beck <alina.beck@mail.com>
2018-01-24 17:04:07 +0800

    rewrite adapter to fetch multiple transactions

--- src/adapters/web3/transactions.js
+++ src/adapters/web3/transactions.js
@@ -1,15 +1,20 @@
 import { getWeb3Instance } from './init';
 
-export function getTransaction(hash, getInstance = getWeb3Instance) {
+export async function getTransactions(transactionNumbers = [], getInstance = getWeb3Instance) {
   const eth = getInstance().eth;
 
-  return new Promise((resolve, reject) => {
-    eth.getTransaction(hash, (err, transaction) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(transaction);
-      }
+  const transactionRequests = transactionNumbers.map(number => {
+    return new Promise(resolve => {
+      eth.getTransaction(number, (err, transaction) => {
+        if (err) {
+          resolve(null);
+        } else {
+          resolve(transaction);
+        }
+      });
     });
   });
+
+  const transactions = await Promise.all(transactionRequests);
+  return transactions.filter(transaction => transaction !== null);
 }
