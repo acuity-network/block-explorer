db02bcf6b7cf7be75da7cca5a9ce4a42712df77f
Alina Beck <alina.beck@mail.com>
2018-01-27 14:18:51 +0800

    improve search input styling

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ import Switcher from './Switcher';
 const App = () => (
   <div className='mix-app'>
     <header className='mix-header'>
-      <h1 className='mix-header__title'>Mix Block Explorer</h1>
+      <h1 className='mix-header__title'>Block Explorer</h1>
       <Search />
     </header>
     <Switcher />
