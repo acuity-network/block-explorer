9b8a06ce5abff370ddae3825590354c5deac888d
Alina Beck <alina.beck@mail.com>
2018-01-31 16:28:36 +0800

    display transaction details

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,21 +1,55 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import Link from 'redux-first-router-link';
 
 import * as selectors from '@/reducers/selectors';
+import * as routes from '@/router';
 
-const mapStateToProps = (state) => {
-  return selectors.getCurrentTransactionForDisplay(state);
-};
+import DetailList from './DetailList';
+import DetailListItem from './DetailListItem';
+import BigNumber from './BigNumber';
 
-const Transaction = ({ from = '', to = '', valueInEther = '' }) => (
+const mapStateToProps = (state) => ({
+  transaction: selectors.getCurrentTransactionForDisplay(state),
+});
+
+const Transaction = ({ transaction }) => (
   <div>
-    <h2>Single Transaction!</h2>
-    <ul>
-      <li>From: {from}</li>
-      <li>To: {to}</li>
-      <li>Value: {valueInEther} Ether</li>
-    </ul>
+    <h2 className='content-block__title list-title'>Transaction Details</h2>
+    <DetailList>
+      <DetailListItem name='Hash' value={transaction.hash} />
+      <DetailListItem
+        name='Block Number'
+        value={<Link to={{
+          type: routes.BLOCK_DETAIL,
+          payload: { blockNumber: transaction.blockNumber }
+        }}>{transaction.blockNumber}</Link>} />
+      <DetailListItem name='Amount' value={`${transaction.valueInEther} Ether`} />
+      <DetailListItem
+        name='Gas Price'
+        value={<BigNumber unit='Wei'>{transaction.gasPriceInWei}</BigNumber>}
+      />
+      <DetailListItem
+        name='Sender'
+        value={<Link to={{
+          type: routes.ACCOUNT_DETAIL,
+          payload: { address: `_${transaction.from}` }
+        }}>{transaction.from}</Link>}
+      />
+      <DetailListItem
+        name='Receiver'
+        value={<Link to={{
+          type: routes.ACCOUNT_DETAIL,
+          payload: { address: `_${transaction.to}` }
+        }}>{transaction.to}</Link>}
+      />
+    </DetailList>
   </div>
 );
+
+Transaction.propTypes = {
+  transaction: PropTypes.object,
+};
 
 export default connect(mapStateToProps)(Transaction);
