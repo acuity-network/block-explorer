963395afd46d54ae571592496c0a77539c671256
Alina Beck <alina.beck@mail.com>
2018-07-10 14:14:41 +0700

    remove redundant promises for accounts, blocks, transactions

--- src/adapters/web3/blocks.js
+++ src/adapters/web3/blocks.js
@@ -1,34 +1,12 @@
 import { getWeb3Instance } from './init';
 
 export function getLatestBlockNumber(getInstance = getWeb3Instance) {
-  const eth = getInstance().eth;
-
-  return new Promise((resolve, reject) => {
-    eth.getBlockNumber((err, number) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(number);
-      }
-    });
-  });
+  return getInstance().eth.getBlockNumber();
 }
 
 export async function getBlocks(blockNumbers = [], getInstance = getWeb3Instance) {
   const eth = getInstance().eth;
+  const blocks = blockNumbers.map(number => eth.getBlock(number));
 
-  const blockRequests = blockNumbers.map(number => {
-    return new Promise(resolve => {
-      eth.getBlock(number, (err, block) => {
-        if (err) {
-          resolve(null);
-        } else {
-          resolve(block);
-        }
-      });
-    });
-  });
-
-  const blocks = await Promise.all(blockRequests);
   return blocks.filter(block => block !== null);
 }
--- src/adapters/web3/transactions.js
+++ src/adapters/web3/transactions.js
@@ -2,19 +2,7 @@ import { getWeb3Instance } from './init';
 
 export async function getTransactions(transactionNumbers = [], getInstance = getWeb3Instance) {
   const eth = getInstance().eth;
+  const transactions = transactionNumbers.map(number => eth.getTransaction(number));
 
-  const transactionRequests = transactionNumbers.map(number => {
-    return new Promise(resolve => {
-      eth.getTransaction(number, (err, transaction) => {
-        if (err) {
-          resolve(null);
-        } else {
-          resolve(transaction);
-        }
-      });
-    });
-  });
-
-  const transactions = await Promise.all(transactionRequests);
   return transactions.filter(transaction => transaction !== null);
 }
